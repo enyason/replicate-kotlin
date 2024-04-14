9fd1299a0c6862bb7d5e981ec74c83719446ccec
MayorJay <joseolu4gsm@yahoo.com>
2024-04-14 23:07:19 +0100

    feat: Add GitHub Actions workflow for code checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
 }
 
 group = "com.github.enyason"
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+
+    @Test
+    fun sampleTest() {
+        assert(8.0.equals(20 / 2.5))
+    }
+}
