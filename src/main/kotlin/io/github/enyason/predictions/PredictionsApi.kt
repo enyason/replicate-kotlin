1f3f79207ffc348eb5dd100c547eb8acd2b078fb
Emmanuel Enya <enyasonjnr@gmail.com>
2024-04-24 10:05:02 +0100

    handle post api processing in PredictionsApi

--- src/main/kotlin/io/github/enyason/predictions/PredictionsApi.kt
+++ src/main/kotlin/io/github/enyason/predictions/PredictionsApi.kt
@@ -2,6 +2,9 @@ package io.github.enyason.predictions
 
 import io.github.enyason.base.ReplicateConfig
 import io.github.enyason.base.RetrofitFactory
+import io.github.enyason.domain.mappers.toPrediction
+import io.github.enyason.domain.models.Prediction
+import io.github.enyason.io.github.enyason.predictions.models.toModel
 import io.github.enyason.predictions.models.PredictionDTO
 import retrofit2.Response
 
@@ -15,6 +18,14 @@ class PredictionsApi(config: ReplicateConfig) {
 }
 
 // Placeholder extension function
-suspend fun PredictionsApi.createPrediction(requestBody: Map<String, Any>): Response<PredictionDTO> {
-    return service.createPrediction(requestBody)
+suspend fun PredictionsApi.createPrediction(requestBody: Map<String, Any>): Prediction {
+    val response = service.createPrediction(requestBody)
+    val predictionDto = response.body()
+    if (response.isSuccessful && predictionDto != null) {
+        return predictionDto.toPrediction()
+    } else {
+        val error = response.errorBody()?.toModel()
+        val message = error?.detail ?: "Could not create predication"
+        throw IllegalStateException(message)
+    }
 }
