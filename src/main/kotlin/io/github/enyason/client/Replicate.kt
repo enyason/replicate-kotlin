170734e0d4031faa916fc155f5632dfecf9790eb
Emmanuel Enya <enyasonjnr@gmail.com>
2024-04-25 16:52:23 +0100

    provide an overload for Replicate.client

--- src/main/kotlin/io/github/enyason/client/Replicate.kt
+++ src/main/kotlin/io/github/enyason/client/Replicate.kt
@@ -1,5 +1,6 @@
 package io.github.enyason.client
 
+import io.github.enyason.base.ReplicateConfig
 import io.github.enyason.domain.models.Prediction
 import io.github.enyason.predictable.Predictable
 import io.github.enyason.predictions.PredictionsApi
@@ -8,6 +9,10 @@ interface Replicate {
     companion object {
         fun client(token: String): Replicate {
             return ReplicateClient(predictionAPI = PredictionsApi(token))
+        }
+
+        fun client(config: ReplicateConfig): Replicate {
+            return ReplicateClient(predictionAPI = PredictionsApi(config))
         }
     }
 
