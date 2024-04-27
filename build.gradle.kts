3f704db88a4de304687603f98ecd3f2ab52dd64b
Emmanuel Enya <enyasonjnr@gmail.com>
2024-04-27 21:46:02 +0100

    code clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1-Beta")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("io.mockk:mockk:1.13.10")
-
 }
 
 tasks.test {
