d203300a3e82d7ac772c51c85939c6d39b4eec2f
MayorJay <joseolu4gsm@yahoo.com>
2024-04-28 11:55:39 +0100

    feat: Code cleanup

--- src/test/kotlin/io/github/enyason/base/RetrofitFactoryTest.kt
+++ src/test/kotlin/io/github/enyason/base/RetrofitFactoryTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.assertThrows
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.test.AfterTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
@@ -24,20 +23,17 @@ class RetrofitFactoryTest {
         assertNotNull(retrofit.baseUrl())
         assertTrue(retrofit.converterFactories().isNotEmpty())
         assertTrue { retrofit.converterFactories().firstOrNull { it is GsonConverterFactory } != null }
-        assertEquals(1, 1)
     }
 
     @Test
     fun `test buildRetrofit _logging is disabled _HttpLoggingInterceptor is not added`() {
         val retrofit = RetrofitFactory.buildRetrofit(ReplicateConfig("token", enableLogging = false))
 
-        assertEquals(1, 1)
         assertTrue { (retrofit.callFactory() as OkHttpClient).interceptors.firstOrNull { it is HttpLoggingInterceptor } == null }
     }
 
     @Test
     fun `test buildRetrofit _baseUrl is not provided _IllegalArgumentException is thrown`() {
-        assertEquals(1, 1)
         assertThrows<IllegalArgumentException>("Should throw an Exception") {
             RetrofitFactory.buildRetrofit(ReplicateConfig("token", baseUrl = ""))
         }
