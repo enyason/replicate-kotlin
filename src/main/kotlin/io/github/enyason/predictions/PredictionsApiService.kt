e6d3044ff7cb88c2336d9ac300f1f9aaf7b05adf
loveotudor <loveotudor@gmail.com>
2024-04-29 21:32:26 +0100

    implement getPrediction

--- src/main/kotlin/io/github/enyason/predictions/PredictionsApiService.kt
+++ src/main/kotlin/io/github/enyason/predictions/PredictionsApiService.kt
@@ -3,13 +3,16 @@ package io.github.enyason.predictions
 import io.github.enyason.predictions.models.PredictionDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 /**
  * The main interface for communicating with [Replicate's](https://replicate.com) Predictions API
  *
  * @author Joseph Olugbohunmi <a href="https://github.com/mayorJAY">link</a>
  * @author Emmanuel Enya <a href="https://github.com/enyason">link</a>
+ * @author Love Otudor <a href="https://github.com/Lamouresparus">link</a>
  */
 interface PredictionsApiService {
 
@@ -20,4 +23,7 @@ interface PredictionsApiService {
     @POST(ENDPOINT)
     @JvmSuppressWildcards
     suspend fun createPrediction(@Body predictionRequest: Map<String, Any>): Response<PredictionDTO>
+
+    @GET("$ENDPOINT/{predictionId}")
+    suspend fun getPrediction(@Path ("predictionId") predictionId: String): Response<PredictionDTO>
 }
