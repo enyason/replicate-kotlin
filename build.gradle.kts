c38ca422e7d7b70e45f56a2a02dbb5fc0adac0c3
MayorJay <joseolu4gsm@yahoo.com>
2024-06-15 20:17:37 +0100

    feat: Add configurations and CI workflow to publish library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,21 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+    id("io.codearte.nexus-staging") version "0.30.0"
+    `java-library`
+    `maven-publish`
+    signing
 }
 
 group = "io.github.enyason"
 version = "1.0-SNAPSHOT"
+val isSnapShotVersion = version.toString().endsWith("SNAPSHOT")
 
 repositories {
     mavenCentral()
+    if (isSnapShotVersion) {
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    }
 }
 
 dependencies {
@@ -30,4 +38,86 @@ tasks.test {
 
 kotlin {
     jvmToolchain(8)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "OSSRH"
+            val releaseRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (isSnapShotVersion) snapshotRepoUrl else releaseRepoUrl)
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "replicate-kotlin"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Replicate Kotlin Client")
+                packaging = "jar"
+                description.set("Kotlin client for Replicate")
+                url.set("https://github.com/enyason/replicate-kotlin")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/mit/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Lamouresparus")
+                        name.set("Love Otudor")
+                        email.set("") // Todo
+                    }
+                    developer {
+                        id.set("Enyason")
+                        name.set("Enya Emmanuel")
+                        email.set("") // Todo
+                    }
+                    developer {
+                        id.set("MayorJay")
+                        name.set("Joseph Olugbohunmi")
+                        email.set("joseolu4gsm@yahoo.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/enyason/replicate-kotlin.git")
+                    developerConnection.set("scm:git:ssh://github.com/enyason/replicate-kotlin.git")
+                    url.set("https://github.com/enyason/replicate-kotlin/tree/main")
+                }
+            }
+        }
+    }
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava8Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
+signing {
+    val signingKey = System.getenv("MAVEN_GPG_PRIVATE_KEY")
+    val signingPassword = System.getenv("MAVEN_GPG_PASSPHRASE")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["mavenJava"])
+}
+
+nexusStaging {
+    serverUrl = if (isSnapShotVersion) "https://s01.oss.sonatype.org/content/repositories/snapshots/" else "https://s01.oss.sonatype.org/service/local/"
+    username = System.getenv("MAVEN_USERNAME")
+    password = System.getenv("MAVEN_PASSWORD")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,15 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("com.gradle.enterprise") version("3.15")
 }
 rootProject.name = "replicate-kotlin"
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
