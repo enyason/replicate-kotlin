cdceda041cb5f428612039e51a7eea89bf9ed6ff
Emmanuel Enya <enyasonjnr@gmail.com>
2024-07-25 06:09:24 +0100

    add gradle configs for maven-central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,83 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm") version "1.9.23"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "io.github.enyason"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
+
+//java {
+//    withJavadocJar()
+//    withSourcesJar()
+//}
+
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "io.github.enyason",
+        artifactId = "replicate-kotlin",
+        version = version.toString()
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("Replicate-Kotlin")
+        description.set("Replicate-Kotlin is a wrapper around Replicate’s API, enabling you to interact with cloud-based AI models using pure Kotlin code. This library is designed to easily integrate generative AI into Android and other kotlin supported environments.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/enyason/replicate-kotlin")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developer information
+        developers {
+            developer {
+                id.set("enyason")
+                name.set("Emmanuel Enya")
+                email.set("enyason@gmail.com")
+            }
+
+            developer {
+                id.set("mayorJAY")
+                name.set("Joseph Olugbohunmi")
+                email.set("joseolu4gsm@yahoo.com")
+            }
+
+            developer {
+                id.set("Lamouresparus")
+                name.set("Love Otudor")
+                email.set("loveotudor@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/enyason/replicate-kotlin")
+            connection.set("scm:git:git://github.com/enyason/replicate-kotlin.git")
+            developerConnection.set("scm:git:ssh://git@github.com/enyason/replicate-kotlin.git")
+        }
+
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
+}
+
+
 
 dependencies {
     // Retrofit and OkHttp3
